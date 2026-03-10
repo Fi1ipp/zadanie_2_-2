611f456f585d1c6b879e1a917ce0fb5495eea263
Fi1ipp <167741273+Fi1ipp@users.noreply.github.com>
2026-03-10 13:25:56 +0100

    commit1

---
+++ otazky.php
@@ -0,0 +1,19 @@
+<?php
+
+$otazky = array(
+    "Čo je vaša oblúbená videohra, a prečo?",
+    "Koľko je 9 + 10?",
+    "Aký ste mali deň?",
+    "Akej značky je vaša mikrovlnka?",
+    "Spell orange.",
+);
+
+$odpovede = array(
+    "Helldivers II pretože znovuodohrateľnosť je takmer nekonečná a je to zábavná hra.",
+    "21",
+    "Celkom dobrý, až na to že som tak skoro vstával.",
+    "Gallet",
+    "The fruit or the color?",
+);
+
+?>
